fe44a450076b0ddb2f67e78a9c82f9168503ded3
GerardoMejia1107 <00104923@uca.edu.sv>
2026-06-06 11:38:32 -0600

    chore: :tada: Initializing SpringBoot project for backend side

---
+++ src/main/java/com/gerardo/swiftentrybackend/SwiftEntryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gerardo.swiftentrybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftEntryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftEntryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gerardo/swiftentrybackend/SwiftEntryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardo.swiftentrybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftEntryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
