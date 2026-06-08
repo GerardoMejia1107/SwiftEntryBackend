c6ed3cf6fac5253321df2012006d021a424520d5
GerardoMejia1107 <00104923@uca.edu.sv>
2026-06-08 14:18:27 -0600

    chore: :rocket: Adding role model

---
+++ src/main/java/com/gerardo/swiftentrybackend/domain/Role/Role.java
@@ -0,0 +1,34 @@
+package com.gerardo.swiftentrybackend.domain.Role;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
