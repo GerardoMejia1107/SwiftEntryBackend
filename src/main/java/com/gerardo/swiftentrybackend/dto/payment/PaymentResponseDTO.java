7fa813e2d76adb1f8502a9d988761c251d175ee8
cherruca <00402523@uca.edu.sv>
2026-06-12 00:11:31 -0600

    :money_with_wings: add: payment dto

---
+++ src/main/java/com/gerardo/swiftentrybackend/dto/payment/PaymentResponseDTO.java
@@ -0,0 +1,31 @@
+package com.gerardo.swiftentrybackend.dto.payment;
+
+import com.gerardo.swiftentrybackend.domain.Payment.enums.PaymentMethod;
+import com.gerardo.swiftentrybackend.domain.Payment.enums.PaymentStatus;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentResponseDTO {
+
+    private Integer id;
+
+    private Integer reservationId;
+
+    private BigDecimal amount;
+
+    private PaymentMethod paymentMethod;
+    private PaymentStatus status;
+
+    private String transactionReference;
+
+    private LocalDateTime paidAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/gerardo/swiftentrybackend/dto/payment/PaymentUpdateDTO.java
@@ -0,0 +1,22 @@
+package com.gerardo.swiftentrybackend.dto.payment;
+
+import com.gerardo.swiftentrybackend.domain.Payment.enums.PaymentStatus;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentUpdateDTO {
+
+    private PaymentStatus status;
+
+    @Size(max = 150, message = "Transaction reference cannot exceed 150 characters")
+    private String transactionReference;
+
+    private LocalDateTime paidAt;
+}
