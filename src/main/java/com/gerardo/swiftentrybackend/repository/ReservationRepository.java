1e520c07b8a2ac58b39bd93b0dfd395c2a934102
cherruca <00402523@uca.edu.sv>
2026-06-11 21:34:10 -0600

    :building_construction: add: reservation repository

---
+++ src/main/java/com/gerardo/swiftentrybackend/repository/ReservationRepository.java
@@ -0,0 +1,26 @@
+package com.gerardo.swiftentrybackend.repository;
+
+
+import com.gerardo.swiftentrybackend.domain.Reservation.ReservationModel;
+import com.gerardo.swiftentrybackend.domain.Reservation.enums.ReservationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationRepository extends JpaRepository<ReservationModel, Integer> {
+
+    List<ReservationModel> findByUserId(Integer userId);
+
+    List<ReservationModel> findByStatus(ReservationStatus status);
+
+    List<ReservationModel> findByStatusAndExpiresAtBefore(
+            ReservationStatus status,
+            LocalDateTime expiresAt
+    );
+
+    Optional<ReservationModel> findByIdAndUserId(Integer id, Integer userId);
+
+    boolean existsByUserIdAndStatus(Integer userId, ReservationStatus status);
+}
