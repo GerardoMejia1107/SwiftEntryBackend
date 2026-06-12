521cdc1aaae2aee5c31fac82cbc8e618c4bb2c71
cherruca <00402523@uca.edu.sv>
2026-06-11 21:51:01 -0600

    :hammer: add: reservation update DTO

---
+++ src/main/java/com/gerardo/swiftentrybackend/dto/ReservationUpdateDTO.java
@@ -0,0 +1,23 @@
+package com.gerardo.swiftentrybackend.dto;
+
+import com.gerardo.swiftentrybackend.domain.Reservation.enums.ReservationStatus;
+import jakarta.validation.constraints.DecimalMin;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReservationUpdateDTO {
+
+    private ReservationStatus status;
+
+    @DecimalMin(value = "0.00", message = "Discount amount cannot be negative")
+    private BigDecimal discountAmount;
+
+    private LocalDateTime purchasedAt;
+}
